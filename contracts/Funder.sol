d2c96aba7d77ef9f5e7d93949f79c9ce13a89bf2
Sidd1121 <siddharthpukale33@gmail.com>
2022-10-26 19:09:57 +0530

    Commit

---
+++ contracts/Funder.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Funder {
+    uint public numoffunders;
+    mapping(uint=>address) private funders;
+    receive() external payable{}
+    function transfer() external payable{
+        funders[numoffunders]=msg.sender;
+
+    }
+    function withdraw(uint withdrawAmount) external{
+        require(withdrawAmount <= 2000000000000000000,"Cannot withdraw more than 2 ether");
+        payable(msg.sender).transfer(withdrawAmount);
+    }
+}
